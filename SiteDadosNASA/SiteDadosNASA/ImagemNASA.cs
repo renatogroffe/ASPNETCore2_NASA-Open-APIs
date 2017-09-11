33fc397d45384c332172e05fd3c3785ad275ad54
Renato Groffe <renato.groffe@yahoo.com.br>
2017-09-10 22:40:47 -0300

    ASP.NET Core 2.0 + NASA Open APIs
    Exemplo de consumo de uma API REST da NASA em uma aplicação ASP.NET Core
    2.0 criada com o Visual Studio 2017.

---
+++ SiteDadosNASA/SiteDadosNASA/ImagemNASA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SiteDadosNASA
+{
+    public class ImagemNASA
+    {
+        public DateTime Data { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Url { get; set; }
+        public string MediaType { get; set; }
+    }
+}
---
+++ SiteDadosNASA/SiteDadosNASA/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace SiteDadosNASA.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string dataImagem { get; set; }
+
+        public void OnGet(
+            [FromServices]IConfiguration config)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                string baseURL =
+                    config.GetSection("NASA_OpenAPIs:BaseURL").Value;
+                string key =
+                    config.GetSection("NASA_OpenAPIs:Key").Value;
+                DateTime dataBase = DateTime.Now.Date.AddDays(
+                    new Random().Next(0, 7) * -1);
+                HttpResponseMessage response = client.GetAsync(
+                    baseURL + "apod?" +
+                    $"api_key={key}&" +
+                    $"date={dataBase.ToString("yyyy-MM-dd")}").Result;
+
+                response.EnsureSuccessStatusCode();
+                string conteudo =
+                    response.Content.ReadAsStringAsync().Result;
+
+                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+
+                ImagemNASA imagem = new ImagemNASA();
+                imagem.Data = dataBase;
+                imagem.Titulo = resultado.title;
+                imagem.Descricao = resultado.explanation;
+                imagem.Url = resultado.url;
+                imagem.MediaType = resultado.media_type;
+
+                TempData["ImagemNASA"] = imagem;
+            }
+        }
+    }
+}
